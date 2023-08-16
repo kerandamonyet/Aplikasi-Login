4bf3a53de70ddd910bf3512f1db7fc82b9f0f791
J A E <99489070+kerandamonyet@users.noreply.github.com>
2023-08-17 00:28:20 +0700

    Update login-script.js
    mengubah () menjadi {}

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -1,32 +1,32 @@
 /** @module Login-Script */
 /** 
 * Membuat variabel loginFormElement untuk tampilan Form.
-* @constant (HTMLElement)
+* @constant {HTMLElement}
 */
 const loginFormElement = document.querySelector('#loginForm');
 
 
 /**
 * Membuat variabel inputEmailElement untuk tampilan input email.
-* @constant (HTMLElement).
+* @constant {HTMLElement}
 */
 const inputEmailElement = document.querySelector('#inputEmail');
 
 /** 
 * Membuat variabel inputPasswordElement untuk tampilan input password.
-* @constant (HTMLElement).
+* @constant {HTMLElement}.
 */
 const inputPasswordElement = document.querySelector('#inputPassword');
 
 /** 
 * Membuat variabel expectedEmail untuk menyimpan informasi email sementara.
-* @constant (string).
+* @constant {string}.
 */
 const expectedEmail = 'admin@dicoding.com';
 
 /**
 * Menbuat variable expectedPassword untuk menyimpan informasi password sementara.
-* @constant (string).
+* @constant {string}.
 */
 const expectedPassword = 'superpassword';
 
@@ -36,13 +36,13 @@ loginFormElement.addEventListener('submit', function(event) {
 
   /**
   * Membuat variabel email untuk menyimpan nilai email yang didapatkan saat button ditekan.
-  * @constant (string)
+  * @constant {string}
   */
   const email = inputEmailElement.value;
 
   /** 
   * Membuat variabel password untuk menyimpan nilai password yang didapatkan saat button ditekan.
-  * @constant (string)
+  * @constant {string}
   */
   const password = inputPasswordElement.value;
 
